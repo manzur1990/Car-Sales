bd1ba506c7c9da39895cd2ae56679716d6e78e22
JM <manzur-1990@hotmail.com>
2020-06-10 09:43:28 -0500

    ADD_FEATURE and REMOVE_FEATURE built

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { carReducer } from './components/reducers/carReducer';
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import { carReducer } from "./reducers/carReducer";
 
-const store = createStore(carReducer)
-console.log("store.getState()", store.getState())
+import App from "./App";
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
+const store = createStore(carReducer);
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    rootElement);
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
